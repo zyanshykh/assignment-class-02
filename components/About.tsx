25f6bd7e000241dfc287788e822810a99fa6dfa3
Zayan-Sheikh <160139662+zyanshykh@users.noreply.github.com>
2024-12-26 05:03:07 +0500

    Class-02 component

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+const About: React.FC = ()=>{
+    return(
+        <section>
+            <h3>This is a About Us Page ^</h3>
+            <p>This is a simple Next.js application using TypeScript.</p>
+        </section>
+    )
+}
+export default About;
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react';
+const Header : React.FC = ()=>{
+    return(
+        <header>
+        <h2>This is Header ` ! ` </h2>
+        </header>
+    );
+};
+export default Header;
